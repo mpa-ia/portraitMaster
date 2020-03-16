231fe012b354d1bac859e6b622c711f26786a682
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-16 18:20:04 +0100

    Store ids of pictures each user voted in db, block possibility to double vote by one user

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const mongoose = require('mongoose');
 const formidable = require('express-formidable');
 const uniqid = require('uniqid');
 const connectToDB = require('./db');
+const requestIp = require('request-ip');
+
 
 // start express server
 const app = express();
+
+app.use(requestIp.mw());
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
@@ -19,11 +24,11 @@ connectToDB();
 app.use(cors());
 app.use(formidable({ uploadDir: './public/uploads/' }, [{
   event: 'fileBegin', // on every file upload...
-    action: (req, res, next, name, file) => {
-      const fileName = uniqid() + '.' + file.name.split('.')[1];
-      file.path = __dirname + '/public/uploads/photo_' + fileName; // ...move the file to public/uploads with unique name
-    }
-  },
+  action: (req, res, next, name, file) => {
+    const fileName = uniqid() + '.' + file.name.split('.')[1];
+    file.path = __dirname + '/public/uploads/photo_' + fileName; // ...move the file to public/uploads with unique name
+  }
+},
 ]));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
